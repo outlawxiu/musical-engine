667c3977ad91b1f2137c1dc7ab9c9f0d0dbaa77f
sunyanfen <13717535156@163.com>
2024-11-06 17:16:01 +0800

    添加歌单|搜索的接口

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import request from "./request";
+
 import type { bannersRes } from "./type";
 
 
@@ -7,3 +8,29 @@ export const getSongsApi = () => {
     return request<bannersRes>({ url: "/banner" });
 }
 
+
+// 所有榜单
+export const toplistApi = () => {
+    return request({ url: '/toplist/detail' })
+}
+
+// 歌单详情
+export const playlistDetailApi = (id: string) => {
+    return request({ url: '/playlist/detail', data: { id } })
+}
+
+
+// 热搜列表
+export const searchHotApi = () => {
+    return request({ url: '/search/hot/detail' })
+}
+
+// 搜索
+export const searchApi = (keywords: string) => {
+    return request({
+        url: '/search',
+        data: {
+            keywords
+        }
+    })
+}
