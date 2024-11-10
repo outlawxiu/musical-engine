7e3923f17a31ed2812112dcb6204e8ec60e7411b
sunyanfen <13717535156@163.com>
2024-11-10 22:38:09 +0800

    修改协议域名端口

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,4 +1,5 @@
-const BaseURL = 'https://zyxcl.xyz/music/api'
+// const BaseURL = 'https://zyxcl.xyz/music/api'
+const BaseURL = 'http://121.89.213.194:5001'
 
 // 接口请求函数的参数类型
 interface RequestParams {
